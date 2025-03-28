2a7c95ce18a65eacb43639ce632f08863f29d920
Subhajit Sarkar <subhajitsarkar.dev@gmail.com>
2025-03-28 10:41:41 +0530

    markedown logic done

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hellow");
+const inputElement = document.getElementById("markdown-input");
+const previewElement = document.getElementById("preview");
+const clearBtn = document.getElementById("clear-btn");
+
+//preview
+inputElement.addEventListener("input", () => {
+  previewElement.innerHTML = marked.parse(inputElement.value);
+});
+
+// clear button
+clearBtn.addEventListener("click", () => {
+  inputElement.value = "";
+  previewElement.innerHTML = "";
+});
